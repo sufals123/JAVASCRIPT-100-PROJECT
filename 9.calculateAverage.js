390934cf196f29fa92a8330328a86a4e92a1e18f
sufal sahoo <77714687+sufals123@users.noreply.github.com>
2024-07-02 10:10:44 +0530

    another method

--- 9.calculateAverage.js
+++ 9.calculateAverage.js
@@ -7,4 +7,13 @@ const calculateAvarage = (arr)=>{
 }
 
 
+const calculateAvarage = (arr) =>{
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+        
+    }
+    return sum / arr.length;
+}
+
 console.log(calculateAvarage([5,5,5]));
