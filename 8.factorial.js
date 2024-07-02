4cfedc4fcf6e9c1147cc546e5e31ce2a292e9d13
sufal sahoo <77714687+sufals123@users.noreply.github.com>
2024-07-02 09:58:34 +0530

    another method

--- 8.factorial.js
+++ 8.factorial.js
@@ -8,4 +8,14 @@ const factorial = (x) => {
     }
     return fact;
 }
+
+const factorial = (num)=>{
+    if(num === 0 || num === 1){
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+
+
+
 console.log(factorial(5)); // Output will be 120
