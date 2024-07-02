31436558b0cbedc83e53c0372be19103d470c37e
sufal sahoo <77714687+sufals123@users.noreply.github.com>
2024-07-02 10:01:04 +0530

    another method

--- 7.findMax.js
+++ 7.findMax.js
@@ -12,6 +12,18 @@ const findMax = (max)=>{
 //     return Math.max(...arr);
 // }
 
+const findMax = (arr)=>{
+    let max = arr[0];
+    for(let i=1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+
+        }
+
+    }
+    return max;
+}
+
 
 console.log(findMax([1,0,3,8,5,6,9,2]))
 console.log(findMax([-2,-3,-4,-5,-1,-7,-8]))
