dd46ada8aa7ec46d34d27c2cf3c266afac531e1e
sufal sahoo <77714687+sufals123@users.noreply.github.com>
2024-07-02 10:02:15 +0530

    another method

--- 5.sortAscending.js
+++ 5.sortAscending.js
@@ -8,5 +8,18 @@ const sortAscending = (arr)=>{
 
 }
 
+function sortAscending(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
 
 console.log(sortAscending([6,5,3,2,10,1]));
